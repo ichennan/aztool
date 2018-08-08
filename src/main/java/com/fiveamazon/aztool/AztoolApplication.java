6bbadd6f98bca2868c791143c7f8cf2d322b20c5
chennan <chennan@unionpayintl.com>
2018-08-08 09:47:55 +0800

    init

---
+++ src/main/java/com/fiveamazon/aztool/AztoolApplication.java
@@ -0,0 +1,12 @@
+package com.fiveamazon.aztool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AztoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AztoolApplication.class, args);
+	}
+}
